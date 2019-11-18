4fe2cf50f12684550dd5d29341fb2bdde4274e89
hanbitkim <47418027+hanbitkim@users.noreply.github.com>
2019-11-18 13:05:55 +0900

    Update WebViewModule.java

--- android/src/main/java/com/github/alinz/reactnativewebviewbridge/WebViewModule.java
+++ android/src/main/java/com/github/alinz/reactnativewebviewbridge/WebViewModule.java
@@ -3,6 +3,7 @@ package com.github.alinz.reactnativewebviewbridge;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
 
 public class WebViewModule extends ReactContextBaseJavaModule {
     public WebViewModule(ReactApplicationContext reactContext) {
@@ -15,14 +16,14 @@ public class WebViewModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void clear() {
+    public void clear(Promise promise) {
         WebViewBridgeManager.webViews.clear();
         WebViewBridgeManager.currentWebViewId = 0;
+        promise.resolve(null);
     }
 
     @ReactMethod
     public void nextWebView() {
-        WebViewBridgeManager.webViews.clear();
         WebViewBridgeManager.currentWebViewId =
         (WebViewBridgeManager.currentWebViewId + 1) % WebViewBridgeManager.webViews.size();
     }
